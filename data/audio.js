2675ad348bfc09f988914055732938cedfd6a4b4
J. Graham <jgraham@Js-MacBook-Air.local>
2023-03-03 02:07:16 -0700

    first commit

--- data/audio.js
+++ data/audio.js
@@ -1,8 +1,8 @@
 
-// const audio = {
-//     Map: new Howl({
-//       src: './audio/map-1.wav',
-//       html5: true
-//     })    
-// }
+const audio = {
+    Map: new Howl({
+      src: './audio/map-1.wav',
+      html5: true
+    })    
+}
 
